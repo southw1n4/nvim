4bbbe62e013683fec3e9069243297c76727c89ac
southw1n4 <101916162+southw1n4@users.noreply.github.com>
2022-06-12 01:43:15 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+
+mkdir .config && mv nvim .config/
+cp .config/.tmux.conf .
+
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
+nvim +PackerSync
