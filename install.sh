5d13d3ad6b4747e1780c6c48c340b12396ea0d42
southw1n4 <101916162+southw1n4@users.noreply.github.com>
2022-06-12 12:56:34 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,5 +4,36 @@ cp .config/nvim/.tmux.conf .
 
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
-
-nvim +PackerSync
+ 
+git clone --depth 1 https://github.com/mfussenegger/nvim-dap\
+ ~/.local/share/nvim/site/pack/packer/start/nvim-dap
+ 
+ git clone --depth 1 https://github.comakinsho/toggleterm.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/toggleterm.nvim
+ 
+ git clone --depth 1 https://github.com/tamton-aquib/staline.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/staline.nvim
+ 
+ git clone --depth 1 https://github.com/andersevenrud/nordic.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/nordic.nvim
+ 
+ git clone --depth 1 https://github.com/kyazdani42/nvim-tree.lua\
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+ 
+ git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/nvim-tree.lua
+ 
+ git clone --depth 1 https://github.com/kyazdani42/nvim-web-devicons\
+ ~/.local/share/nvim/site/pack/packer/start/nvim-web-devicons
+ 
+  git clone --depth 1 https://github.com/akinsho/bufferline.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/bufferline.nvim
+ 
+  git clone --depth 1 https://github.com/neoclide/coc.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/coc.nvim
+ 
+  git clone --depth 1 https://github.com/iamcco/markdown-preview.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/markdown-preview.nvim
+ 
+   git clone --depth 1 https://github.com/iamcco/mathjax-support-for-mkdp\
+ ~/.local/share/nvim/site/pack/packer/start/mathjax-support-for-mkdp
